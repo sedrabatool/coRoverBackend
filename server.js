8127dc13ff8bfb50ca7f861ec9ee9a5639349a3b
sidra <batoolsidra837@gmail.com>
2026-06-14 16:27:01 +0500

    updated

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Firebase Initialization
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore();
