c9f1927a81a81b9fca6b7f7808cb567c9456f655
sidra <batoolsidra837@gmail.com>
2026-06-14 15:33:38 +0500

    updated

--- server.js
+++ server.js
@@ -290,6 +290,8 @@ app.post("/verify-otp", async (req, res) => {
 /* =====================================================
    START SERVER
 ===================================================== */
-app.listen(5000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
